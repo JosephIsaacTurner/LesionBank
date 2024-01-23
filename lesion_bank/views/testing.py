e696cd8112a04fc1193125368394483cb477013e
Joseph Turner <josephisaacturner@gmail.com>
2024-01-23 16:27:44 -0500

    shorten table

--- lesion_bank/views/testing.py
+++ lesion_bank/views/testing.py
@@ -7,6 +7,9 @@ def test_view(request):
     nifti_handler = NiftiHandler()
     nifti_handler.populate_data_from_s3(path)
     df_xyz, df_voxel_id = nifti_handler.nd_array_to_pandas()
+    # Now let's trim the dataframes to only the first 10 rows
+    df_xyz = df_xyz.iloc[:10]
+    df_voxel_id = df_voxel_id.iloc[:10]
     error_messages = []
     info_messages = []
     info_messages.append(f"df_xyz_html: {df_xyz.to_html()}")
